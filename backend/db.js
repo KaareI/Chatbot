a164d6eaef17c37a0fd52a4768fd52f836ec5564
Kaarel <karlkaareltaniel.seppel@gmail.com>
2024-02-19 17:02:25 +0200

    Login logic 90% done

---
+++ backend/db.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql2');
+
+// Load environment variables from .env file
+require('dotenv').config();
+
+// Access environment variables
+const database = process.env.DB_DATABASE;
+const host = process.env.DB_HOST;
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+// Database connection
+const connection = mysql.createConnection({
+    host,
+    user,
+    password,
+    database,
+});
+
+// Creating connection
+connection.connect((err) => {
+    if (err) {
+        console.error('Connected to MySQL database ID: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL database ID: ' + connection.threadId);
+});
+
+module.exports = connection;
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
+const db = require('./db')
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = 5000;
 
 const corsOptions = {
     origin: 'http://localhost:3000',
@@ -24,3 +25,41 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+// Compare passwords
+async function comparePassword(plaintextPassword, hash) {
+    const result = await bcrypt.compare(plaintextPassword, hash);
+    return result;
+}
+
+// Login logic
+app.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+
+    // Retrieve the hashed password from the database based on the username
+    const query = process.env.LOGIN_QUERY;
+
+    db.query(query, [username], async (err, results) => {
+        if (err) {
+            console.error('Error querying the database:', err);
+            return res.status(500).json({error: 'Error querying the database:'});
+        }
+
+        if (results.length === 0) {
+            return res.status(401).json({error: 'Invalid username'});
+        }
+
+        const hashedPassword = results[0].User_Password;
+
+        // Compare the provided password with the hashed password from the database
+        const passwordsMatch = await comparePassword(password, hashedPassword);
+
+        if (passwordsMatch) {
+            // Authentication successful
+            req.session.isAuthenticated = true;
+            res.json(true);
+        } else {
+            // Authentication failed
+            res.status(401).json({error: 'Invalid credentials'});
+        }
+    });
+});
